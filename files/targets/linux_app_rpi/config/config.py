d41ba205114b2e6270473bf72350d4c57de4037a
Paulo Coutinho <paulo@prsolucoes.com>
2019-03-24 00:41:35 -0300

    fix linux travis and add linux app for raspberry pi

---
+++ files/targets/linux_app_rpi/config/config.py
@@ -0,0 +1,12 @@
+def run(params):
+    return {
+        'project_name': 'Sample',
+        'build_types': ['Debug', 'Release'],
+        'archs': [
+            {
+                'arch': 'armv7',
+                'conan_arch': 'armv7',
+                'conan_profile': 'ezored_linux_app_rpi_profile',
+            },
+        ],
+    }
