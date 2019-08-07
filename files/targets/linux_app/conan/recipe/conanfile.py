5c1ad8f3dbe32201ae2df6dd0f8a631227c24dc3
Paulo Coutinho <paulo@prsolucoes.com>
2019-08-07 04:05:39 -0300

    rollback sdl version to 2.0.9

--- files/targets/linux_app/conan/recipe/conanfile.py
+++ files/targets/linux_app/conan/recipe/conanfile.py
@@ -41,7 +41,7 @@ class TargetConan(ConanFile):
         cmake.build()
 
     def requirements(self):
-        self.requires('sdl2/2.0.10@bincrafters/stable')
+        self.requires('sdl2/2.0.9@bincrafters/stable')
         self.requires('sdl2_image/2.0.5@bincrafters/stable')
         self.requires('sdl2_ttf/2.0.15@bincrafters/stable')
         self.requires('libpng/1.6.36@bincrafters/stable')
