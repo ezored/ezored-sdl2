0ea33735610099858fb1521a1149d40e515cf6cd
Paulo Coutinho <paulo@prsolucoes.com>
2019-08-09 21:11:39 -0300

    macos only support x86_64

--- files/config/target_macos_app.py
+++ files/config/target_macos_app.py
@@ -4,12 +4,6 @@ import os
 def run(proj_path, target_name, params):
     archs = [
         {
-            'arch': 'x86',
-            'conan_arch': 'x86',
-            'conan_profile': 'ezored_macos_app_profile',
-            'min_version': '10.9',
-        },
-        {
             'arch': 'x86_64',
             'conan_arch': 'x86_64',
             'conan_profile': 'ezored_macos_app_profile',
