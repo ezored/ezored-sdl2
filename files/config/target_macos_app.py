e52699315ca84f12f27b5a23bf1f251c367159d2
Paulo Coutinho <paulo@prsolucoes.com>
2019-11-22 16:20:50 -0300

    code update, code format, updated dependencies, ci update and conan update

--- files/config/target_macos_app.py
+++ files/config/target_macos_app.py
@@ -4,28 +4,30 @@ import os
 def run(proj_path, target_name, params):
     archs = [
         {
-            'arch': 'x86_64',
-            'conan_arch': 'x86_64',
-            'conan_profile': 'ezored_macos_app_profile',
-            'min_version': '10.9',
-        },
+            "arch": "x86_64",
+            "conan_arch": "x86_64",
+            "conan_profile": "ezored_macos_app_profile",
+            "min_version": "10.9",
+        }
     ]
 
-    if os.getenv('CONAN_ARCHS'):
+    if os.getenv("CONAN_ARCHS"):
         archs = []
-        env_archs = os.getenv('CONAN_ARCHS').split(',')
+        env_archs = os.getenv("CONAN_ARCHS").split(",")
 
         for it in env_archs:
-            archs.append({
-                'arch': it,
-                'conan_arch': it,
-                'conan_profile': 'ezored_macos_app_profile',
-                'min_version': '10.9',
-            })
+            archs.append(
+                {
+                    "arch": it,
+                    "conan_arch": it,
+                    "conan_profile": "ezored_macos_app_profile",
+                    "min_version": "10.9",
+                }
+            )
 
     return {
-        'project_name': 'Sample',
-        'build_types': ['Debug', 'Release'],
-        'archs': archs,
-        'assets_dir': 'projects/samples/sdl2/assets',
+        "project_name": "Sample",
+        "build_types": ["Debug", "Release"],
+        "archs": archs,
+        "assets_dir": "projects/samples/sdl2/assets",
     }
